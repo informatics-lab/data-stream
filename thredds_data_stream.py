aa2e8d5b4d21beb8c5f761cff728adb1d4ba963f
Simon Stanley <simon.stanley@informaticslab.co.uk>
2015-06-22 15:55:16 +0100

    Added working example

--- thredds_data_stream.py
+++ thredds_data_stream.py
@@ -40,7 +40,7 @@ def read_requests():
                     if request_dict is not None:
                         req_params = line.split("=")
                         if len(req_params) != 2:
-                            raise UserWarning("Incorrect request parameter "/
+                            raise UserWarning("Incorrect request parameter "\
                                               "'%s', line %s."
                                               % (line, i+1))
                         if req_params[0] not in valid_req_params:
@@ -51,6 +51,10 @@ def read_requests():
                             print ("Ignoring incomplete request parameter '%s'"
                                    % line)
                         else:
+                            # Convert to list if appropriate.
+                            req_params[1] = req_params[1].split(",")
+                            if len(req_params[1]) == 1:
+                                req_params[1] = req_params[1][0]
                             request_dict[req_params[0]] = req_params[1]
     return requests
 
@@ -58,13 +62,16 @@ def main():
     """
 
     """
-    api_key = ""
+    api_key = "4fc1f5e2-00f9-4ef2-a252-e5c3e9af1734"
     req = WCS2Requester(api_key)
 
     requests = read_requests()
 
     for request_dict in requests:
         response = req.getCoverage(**request_dict)
+        with open("test.nc", "w") as outfile:
+            outfile.write(response.content)
+
 
 if __name__ == "__main__":
     main()
