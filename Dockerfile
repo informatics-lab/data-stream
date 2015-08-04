2c0da881f3e456ae00ec1c2068c918948d7d2da8
Niall Robinson <niall.h.robinson@gmail.com>
2015-08-04 12:52:55 +0100

    added setup py

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN cd ogc && python setup.py install
 RUN git clone https://github.com/met-office-lab/beta-data-services bds
 RUN cd bds && python setup.py install
 
-ADD [^.]* ./ 
+ADD [^.]* ./
+RUN python setup.py install
 
 CMD python thredds_data_stream.py
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='data-stream',
+      version='0.1.0',
+      install_requires=["requests >= 2.3.0",
+                        "boto",
+                        "python-dateutil"],
+      description='Stream data from betadata services to THREDDs',
+      author='Met Office Informatics Lab',
+      maintainer='Met Office Informatics Lab',
+      url='https://github.com/met-office-lab/data-stream',
+      license='TBC',
+      packages=['data-stream'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ]
+     )
